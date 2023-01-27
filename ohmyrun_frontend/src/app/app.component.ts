bb8ccac3cc0e4b73182c84fa5e26d17f8c1ab052
Carmen Vega <cvega23@horizon.csueastbay.edu>
2023-01-27 10:57:15 -0800

    Removed hello world code

--- ohmyrun_frontend/src/app/app.component.ts
+++ ohmyrun_frontend/src/app/app.component.ts
@@ -8,14 +8,3 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ohmyrun_frontend';
 }
-
-@Component({
-  selector: 'hello-world',
-  template: `
-    <h2>Hello World</h2>
-    <p>This is my first component!</p>
-  `
-})
-export class HelloWorldComponent {
-  // The code in this class drives the component's behavior.
-}
