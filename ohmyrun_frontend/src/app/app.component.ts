c14e5680521565621b1c70fa269f9ad08dc56adb
echo "PATH=/opt/homebrew/bin:$PATH\nexport PATH"  /Users/$(whoami)/.zprofile <PATH="/opt/homebrew/bin:$PATH>
2023-01-30 09:33:39 -0800

    Connect Google Maps JS API to display a map

--- ohmyrun_frontend/src/app/app.component.ts
+++ ohmyrun_frontend/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
+// import { styles } from './mapstyles';
+// import * as dotenv from 'dotenv';
+// dotenv.config();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ohmyrun_frontend';
+
+  // private map: google.maps.Map
+
+  ngOnInit(): void {
+    let loader = new Loader({
+      apiKey: 'MYKEY'
+    })
+
+    loader.load().then(() => {
+      console.log('loaded gmaps')
+
+      const location = { lat: 47.685128247952946, lng: -122.33719484786576}
+
+      new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: location,
+        zoom: 6,
+      })
+    })
+  }
 }
