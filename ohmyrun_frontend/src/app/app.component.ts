9f2a1fc75d122166eca1127d69414ae21177a2e0
Carmen Vega <cvega23@horizon.csueastbay.edu>
2023-02-01 09:42:01 -0800

    Added functionality to alow users to place multiple pins on the map

--- ohmyrun_frontend/src/app/app.component.ts
+++ ohmyrun_frontend/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
-// import { styles } from './mapstyles';
-// import * as dotenv from 'dotenv';
-// dotenv.config();
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -13,20 +11,18 @@ export class AppComponent implements OnInit {
 
   title = 'ohmyrun_frontend';
 
-  // private map: google.maps.Map
+  markers: google.maps.Marker[] = [];
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     let loader = new Loader({
-      apiKey: 'API'
+      apiKey: 'API KEY'
     })
 
     loader.load().then(() => {
       console.log('loaded gmaps')
      
-      // const location = {
-      //   lat: 50.684855,
-      //   lng: 122.337323
-      // }
       if(!navigator.geolocation) {
         console.log("location is not supported")
       } 
@@ -35,10 +31,39 @@ export class AppComponent implements OnInit {
         const location = {
           lat: position.coords.latitude, lng: position.coords.longitude
         }
+        const map = 
         new google.maps.Map(document.getElementById("map") as HTMLElement, {
           center: location,
           zoom: 15,
-        })
         });
-    })
-}}
+
+        map.addListener('click', (event: google.maps.MapMouseEvent) => {
+          if (event.latLng) {
+          console.log(event.latLng.lat(), event.latLng.lng());
+          this.addMarker(event.latLng, map);
+          }
+        });
+      });
+    });
+  }
+
+  addMarker(location: google.maps.LatLng, map: google.maps.Map) {
+    const marker = new google.maps.Marker({
+      position: location,
+      map: map,
+    });
+    this.markers.push(marker);
+  }
+
+  saveMarkers() {
+       // Code to save the markers to a database or local storage.
+       this.http.post('http://127.0.0.1:5000/pin', this.markers).subscribe(
+        (data) => {
+          console.log('Successfully saved the markers to the database');
+        },
+        (error) => {
+          console.error('An error occurred while saving the markers to the database: ', error);
+        }
+      );
+  }
+} 
