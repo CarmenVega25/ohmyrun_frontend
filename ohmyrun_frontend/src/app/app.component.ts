526ef7dcd053af4baad25214246e4a3b8d8b7c66
Melissa <ma.pena94@gmail.com>
2023-02-08 11:20:49 -0800

    small change

--- ohmyrun_frontend/src/app/app.component.ts
+++ ohmyrun_frontend/src/app/app.component.ts
@@ -38,7 +38,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     let loader = new Loader({
-      apiKey: 'AIzaSyD_rpFGfqEPwCj-WYoFMRXse8QZdCheJEI',
+      apiKey: 'API KEY',
     });
     
 
