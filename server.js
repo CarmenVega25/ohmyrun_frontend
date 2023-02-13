f2d0cbabdaa6d2f599647ddf845e565ca6fe8944
Tatiana Obando <PATH="/opt/homebrew/bin:$PATH>
2023-02-13 10:18:36 -0800

    Added Server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/ohmyrun'));
+app.use(express.static('./dist/ohmyrun_frontend'));
 
 app.get('/*', function(req, res) {
-res.sendFile('index.html', {root: 'dist/ohmyrun/'});
+res.sendFile('index.html', {root: 'dist/ohmyrun_frontend/'});
 });
 
 app.listen(process.env.PORT || 8080);
