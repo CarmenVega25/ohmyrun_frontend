3ab2e277568f1d9e006542d9dde454c290f798ca
Tatiana Obando <PATH="/opt/homebrew/bin:$PATH>
2023-02-13 11:46:14 -0800

    deleted Procfile and match dist route

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/ohmyrun-frontend'));
+app.use(express.static('./dist/ohmyrun_frontend'));
 
 app.get('/*', function(req, res) {
-res.sendFile('index.html', {root: 'dist/ohmyrun-frontend'});
+res.sendFile('index.html', {root: 'dist/ohmyrun_frontend'});
 });
 
 app.listen(process.env.PORT || 8080);
